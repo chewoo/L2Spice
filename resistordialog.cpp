055553cd70b3d4417d049d0abcead40429cfb2df
dongdong122~ <leehero724@gmail.com>
2023-03-23 02:50:18 +0800

    fixed input value bug

--- resistordialog.cpp
+++ resistordialog.cpp
@@ -10,6 +10,9 @@ Resistordialog::Resistordialog(QWidget *parent) :
 
     QObject::connect(ui->OK,SIGNAL(clicked()),this,SLOT(buttonOK()));
     QObject::connect(ui->Cancel,SIGNAL(clicked()),this,SLOT(buttonCancel()));
+    ui->warning->hide();
+
+    setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint);
 
 }
 
@@ -20,17 +23,27 @@ Resistordialog::~Resistordialog()
 
 QString Resistordialog::getInput()
 {
-    return text;
+    if(text != "")
+        return text;
+    else
+        return "0";
 }
 
 void Resistordialog::buttonOK()
 {
-    text = ui->value->text();
-    this->hide();
+    if(ui->value->text() != "")
+    {
+        text = ui->value->text();
+        ui->warning->hide();
+        this->hide();
+    }
+    else
+        ui->warning->show();
 }
 
 void Resistordialog::buttonCancel()
 {
     ui->value->setText(text);
+    ui->warning->hide();
     this->hide();
 }
